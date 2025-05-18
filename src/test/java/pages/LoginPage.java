aba196c3ddc1ddb4ab57f672f756a93e23251072
Aduduluanku <gohanssj524@gmail.com>
2025-05-18 11:19:27 +0700

    fix

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -2,7 +2,8 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-<<<<<<< HEAD
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 
 public class LoginPage {
 
@@ -22,16 +23,6 @@ public class LoginPage {
     public void loginButton(){
         driver.findElement(By.xpath("//button[text()='Login']")).click();
     }
-}
-=======
-import org.openqa.selenium.WebElement;
-import org.testng.Assert;
-
-public class LoginPage {
-    WebDriver driver;
-    public LoginPage(WebDriver driver) {
-        this.driver = driver;
-    }
 
     public void login(String userName, String pwd){
         driver.findElement(By.xpath("//input[@id=\"emailOrUsername\"]")).sendKeys(userName);
@@ -41,4 +32,7 @@ public class LoginPage {
 }
 
 
->>>>>>> master
+
+
+
+
